c842474b65acfbbc2b33a4a524d1a27999c6b7f4
Beyza Nur Karadana <83921230+BeyzaNurKaradana@users.noreply.github.com>
2024-08-11 01:46:09 +0300

    added table prototype

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import './App.css'
 
 function App() {
@@ -6,7 +6,40 @@ function App() {
 
   return (
     <>
-      
+      <div className="container mx-auto">
+      <div className="flex flex-col mt-9 border border-gray-100 rounded">
+        <table className="w-full table-auto border-gray-100">
+          <thead className="bg-slate-100 border-b ">
+            <tr className="opacity-40 ">
+              <th className="text-left">Crypto</th>
+              <th className="text-right">Price</th>
+              <th className="text-right">Market Value</th>
+              <th>24h Change</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+              <tr className="text-center" >
+                <td className="py-4 text-left flex leading-5">
+                  gdfgfd
+                </td>
+                <td className="py-4 text-right">
+                  sdfdf
+                </td>
+
+                <td className="py-4 text-right">
+                  dfdfdf
+                </td>
+                <td className="py-4">
+                  sdfdsf
+                </td>
+              </tr>
+          </tbody>
+        </table>
+        
+      </div>
+
+    </div>
     </>
   )
 }
